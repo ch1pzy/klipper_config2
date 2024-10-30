82242072b86e5d984319f72b79e410167c767cd8
ch1pzy <>
2024-10-30 15:45:29 +1100

    backup 2024-10-30

--- scripts/firmware_flash_all.sh
+++ scripts/firmware_flash_all.sh
@@ -5,21 +5,23 @@ cd ~/klipper
 git pull
 
 
-make clean KCONFIG_CONFIG=~/klipper_config/config.robinE3
-make KCONFIG_CONFIG=~/klipper_config/config.robinE3
-./scripts/flash-sdcard.sh /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 mks-robin-e3
+#make clean KCONFIG_CONFIG=~/klipper_config/config.rpi
+#make KCONFIG_CONFIG=~/klipper_config/config.rpi
+#make flash KCONFIG_CONFIG=~/klipper_config/config.rpi
 
-make clean KCONFIG_CONFIG=~/klipper_config/config.rpi
-make KCONFIG_CONFIG=~/klipper_config/config.rpi
-make flash KCONFIG_CONFIG=~/klipper_config/config.rpi
+#make clean KCONFIG_CONFIG=~/klipper_config/config.GT2560
+#make KCONFIG_CONFIG=~/klipper_config/config.GT2560
+#avrdude -carduino -patmega1280 -P/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A904Y7PU-if00-port0  -b57600 -D -Uflash:w:out/klipper.elf.hex:i
 
-make clean KCONFIG_CONFIG=~/klipper_config/config.GT2560
-make KCONFIG_CONFIG=~/klipper_config/config.GT2560
-avrdude -carduino -patmega1280 -P/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A904Y7PU-if00-port0  -b57600 -D -Uflash:w:out/klipper.elf.hex:i
+make clean KCONFIG_CONFIG=~/klipper_config//config.sb2040
+make KCONFIG_CONFIG=~/klipper_config/config.sb2040 -j4
+python3 ~/Katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 94ed859879a5
 
-make clean KCONFIG_CONFIG=~/klipper_config//config.EBBcan
-make KCONFIG_CONFIG=~/klipper_config//config.EBBcan
-python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u 60b51521b7ac
+make clean KCONFIG_CONFIG=~/klipper_config/config.dp5.can
+make KCONFIG_CONFIG=~/klipper_config/config.dp5.can -j4
+# Need a pause here to notify that the board needs to be put into DFU Mode... just include the below into a bash script and give the user the instruction to run it after the're in DFU
+make KCONFIG_CONFIG=config.dp5.can flash FLASH_DEVICE=0483:df11
+#./scripts/flash-sdcard.sh /dev/serial/by-id/usb-1a86_USB_Serial-if00-port0 mks-robin-e3
 
 #make clean KCONFIG_CONFIG=~/klipper_config/config.arduinonano
 ##make menuconfig KCONFIG_CONFIG=~/klipper_config/config.arduinonano
